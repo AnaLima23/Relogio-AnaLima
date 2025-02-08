7660ad7e737bc2b37f0142344a447c60b297e5be
AnaLima23 <151173124+AnaLima23@users.noreply.github.com>
2025-02-08 09:50:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,22 @@ function atualizarRelogio() {
     let minutos = agora.getMinutes();
     let segundos = agora.getSeconds();
 
-    let grausSegundos = segundos * 6;
-    let grausMinutos = minutos * 6 + segundos * 0.1;
-    let grausHoras = horas * 30 + minutos * 0.5;
+  
+    let grausSegundos = (segundos * 6)-90; 
+    let grausMinutos = (minutos * 6 + segundos * 0.1) -90;
+    let grausHoras = ((horas % 12) * 30 + minutos * 0.5)-90; 
 
+ 
     document.querySelector('.p_s').style.transform = `rotate(${grausSegundos}deg)`;
     document.querySelector('.p_m').style.transform = `rotate(${grausMinutos}deg)`;
     document.querySelector('.p_h').style.transform = `rotate(${grausHoras}deg)`;
 
+    
     document.querySelector('.digital').textContent = 
         `${horas.toString().padStart(2, '0')}:${minutos.toString().padStart(2, '0')}:${segundos.toString().padStart(2, '0')}`;
 }
-setInterval(atualizarRelogio, 1000);   
+
+
+setInterval(atualizarRelogio, 1000);
+
 atualizarRelogio();
